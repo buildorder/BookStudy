b64527faec49e3e5f82e4e187f43a8fc01b8560e
Min Seok Kim <kms9534@gmail.com>
2021-02-06 16:17:51 +0900

    태스크 생성 및 할당 기능 추가

--- 임베디드_OS_개발_프로젝트/boot/Main.c
+++ 임베디드_OS_개발_프로젝트/boot/Main.c
@@ -9,6 +9,10 @@ static void Hw_init(void);
 static void Printf_test(void);
 static void Timer_test(void);
 
+void User_task0(void);
+void User_task1(void);
+void User_task2(void);
+
 void main(void)
 {
 	Hw_init();
@@ -56,4 +60,50 @@ static void Timer_test(void)
 		debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
 		delay(1000);
 	}
+}
+
+static void Kernel_init(void)
+{
+	uint32_t taskId;
+
+	Kernel_task_init();
+
+	taskId = Kernel_task_create(User_task0);
+	if (NOT_ENOUGH_TASK_NUM == taskId)
+	{
+		putstr("Task0 creation fail\n");
+	}
+	
+	taskId = Kernel_task_create(User_task1);
+	if (NOT_ENOUGH_TASK_NUM == taskId)
+	{
+		putstr("Task0 creation fail\n");
+	}
+	
+	taskId = Kernel_task_create(User_task2);
+	if (NOT_ENOUGH_TASK_NUM == taskId)
+	{
+		putstr("Task0 creation fail\n");
+	}
+}
+
+void User_task0(void)
+{
+	debug_printf("User Task #0\n");
+
+	while(1);
+}
+
+void User_task1(void)
+{
+	debug_printf("User Task #1\n");
+
+	while(1);
+}
+
+void User_task2(void)
+{
+	debug_printf("User Task #2\n");
+
+	while(1);
 }
